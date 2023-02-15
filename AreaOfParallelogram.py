f02bd403e2fb91db9aeda127668eadf3fb6dc94e
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-15 10:35:33 +0530

    Create AreaOfParallelogram.py

---
+++ AreaOfParallelogram.py
@@ -0,0 +1,6 @@
+base = float(input("Enter the base of the parallelogram: "))
+height = float(input("Enter the height of the parallelogram: "))
+
+area = base * height
+
+print("The area of the parallelogram is:", area)
