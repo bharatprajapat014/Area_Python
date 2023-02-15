077e73b9ea99f6fcbe401a2300ef2cf7523146f5
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-15 10:30:02 +0530

    Create  AreaOfRhombus.py

---
+++ AreaOfRhombus.py
@@ -0,0 +1,8 @@
+# Program to calculate the area of a rhombus
+
+diagonal1 = float(input("Enter the length of diagonal 1: "))
+diagonal2 = float(input("Enter the length of diagonal 2: "))
+
+area = (diagonal1 * diagonal2) / 2
+
+print(f"The area of the rhombus is {area}.")
